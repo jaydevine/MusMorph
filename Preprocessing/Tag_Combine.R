638059d4b01238ac2b0e26fb8b3d855ed625625d
Jay Devine <jay.devine1@ucalgary.ca>
2024-03-06 18:28:21 -0700

    Improved readability of script.

--- Preprocessing/Tag_Combine.R
+++ Preprocessing/Tag_Combine.R
@@ -1,7 +1,20 @@
 # This is an R script that will accept two input .tag files (with >=4  homologous init. markers) and combine them into a two volume tag file that will be converted into a rigid transformation matrix. 
 
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# You only need to edit the variables within these dashed lines.
+
+# Define the path to thespecimen list file.
+FILENAME="/path/to/<PROJECT>/Source/spec_list.txt"
+# Define the path to the .tag files.
+Source_Tag_path="/path/to/<PROJECT>/Source/Tag"
+# Define the name of your reference specimen or atlas, excluding the file extension. 
+Atlas=""
+# Define your .txt file of specimen names, say spec_list.txt.
+All_Specimens=read.table(FILENAME,stringsAsFactors=FALSE)
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 # Define tag combine function. 
-Combine_Tag=function(Source_XFM_path,Atlas,SpecID){
+Combine_Tag=function(Source_Tag_path,Atlas,SpecID){
   Atlas_LMs=read.table(paste(Source_Tag_path,paste(Atlas,".tag",sep=""),sep="/"), sep=c(" ",";"), skip=4,header=FALSE,stringsAsFactors=FALSE)[,2:4]
   Spec_LMs=read.table(paste(Source_Tag_path,paste(SpecID,".tag",sep=""),sep="/"), sep=c(" ",";"), skip=4,header=FALSE,stringsAsFactors=FALSE)[,2:4]
   N_Atlas_LMs=nrow(Atlas_LMs)
@@ -20,16 +33,8 @@ Combine_Tag=function(Source_XFM_path,Atlas,SpecID){
   }
 }
 
-# Define the directory with the specimen list file (below) and where the combined .tag files will end up. 
-Source_MNC_path="/path/to/<PROJECT>/Source/MNC"
-# Define the path to the .tag files.
-Source_Tag_path="/path/to/<PROJECT>/Source/Tag"
-# Define the name of your reference specimen or atlas, excluding the file extension. 
-Atlas=""
-# Define your .txt file of specimen names, say spec_list.txt.
-All_Specimens=read.table(paste(Source_MNC_path,"spec_list.txt",sep="/"),stringsAsFactors=FALSE)
 setwd(Source_Tag_path)
 # Run the tag combine function and output the files to Source_MNC_path. 
 for(j in 1:nrow(All_Specimens)){
-  Combine_Tag(Source_MNC_path,Atlas,All_Specimens[j,1])
+  Combine_Tag(Source_Tag_path,Atlas,All_Specimens[j,1])
 }
